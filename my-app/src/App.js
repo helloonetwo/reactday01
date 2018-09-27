e5ec7c0a7d0cd16ef6a4cca2814e7e5eef92d53c
zxw <1774761877@qq.com>
2018-09-28 00:07:22 +0800

    react 的双向数据绑定

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,32 @@
+//引入React，Component
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+//组件继承component
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      day:'sunday'
+    }
+  }
+  change(e) {
+    console.log(e.target.value);
+    //赋值，将input 的值赋给this.state.day
+    this.state.day=e.target.value;
+    //通知视图更新
+    this.setState({
+
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      //保证一个跟节点
+      <div>
+          hello  world
+          <br></br>
+         {this.state.day}
+           <br></br>
+          <input type="text"  value={this.state.day}  onChange={  (e)=>{this.change(e)}  }/>
       </div>
     );
   }
